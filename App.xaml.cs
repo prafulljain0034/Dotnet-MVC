00390863990145587baee0fd559dd1dd3088753e
Prafull Jain <prafull.jain@anktech.co.in>
2025-06-19 17:26:18 +0530

    add App.xaml file

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MyMauiApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+		MainPage = new AppShell(); // << this loads the TabBar
+
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
